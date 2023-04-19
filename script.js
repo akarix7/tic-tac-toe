a0a3abc55e45280dec40d87a6dd853707edaa2e9
akarix7 <116602617+akarix7@users.noreply.github.com>
2023-04-19 17:19:12 -0500

    create module > createBoard

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const Gameboard = () => {
     let gameArr = [];
+    let gameDiv = document.querySelector(".game");
+
+    function _createBoard(){
+        for(let i = 0; i < 8; i++){
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.dataset.id = (i).toString();
+        }
+
+    }
 }
