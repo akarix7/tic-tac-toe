143113f76ca065c79eea63b243b2ced2cc933b79
akarix7 <116602617+akarix7@users.noreply.github.com>
2023-04-24 21:27:37 -0500

    added: init, add createBoard DOM

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const Gameboard = () => {
+const Gameboard = (() => {
     let gameArr = [];
     let gameDiv = document.querySelector(".game");
 
+    const init = () => {
+        _createBoard();
+    }
+
     function _createBoard(){
-        for(let i = 0; i < 8; i++){
+        for(let i = 0; i < 9; i++){
             let cell = document.createElement("div");
             cell.className = "cell";
             cell.dataset.id = (i).toString();
+            if(i % 2 === 0) {
+                cell.textContent = "x";
+            }else{
+                cell.textContent = "o";
+            }
+            gameDiv.appendChild(cell);
         }
-
     }
-}
+    return {
+        init,
+    }
+})();
+
+Gameboard.init();
