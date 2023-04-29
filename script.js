a8ec90ae9e517b46876ec50ed60ea11f82671bed
akarix7 <116602617+akarix7@users.noreply.github.com>
2023-04-28 21:01:14 -0500

    adding board style to frontend

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ const Player = (name, piece) => {
 }
 
 const Gameboard = (() => {
-    let gameArr = [["x", "o", "x"], ["o", "x", "o"], ["x", "o", "x"]];
+    //let gameArr = [["x", "o", "x"], ["o", "x", "o"], ["x", "o", "x"]];
+    //let gameArr = [["", "", ""], ["", "", ""], ["","",""]];
+    let gameArr = new Array(3);
     let gameDiv = document.querySelector(".game");
 
     const init = () => {
@@ -14,7 +16,9 @@ const Gameboard = (() => {
         _render();
     }
 
-    function _createBoard(){
+    const getGameArr = () => gameArr;
+
+    const _createBoard = () => {
         for(let i = 0; i < 9; i++){
             let cell = document.createElement("div");
             cell.className = "cell";
@@ -23,28 +27,41 @@ const Gameboard = (() => {
         }
     }
 
-    function _render(){
+    const _render = () => {
         let index = 0;
         for(let i = 0; i < 3; i++){
+            gameArr[i] = new Array(3);
             for(let j = 0; j < 3; j++){
                 // console.log(gameDiv.children.item(index).dataset.id);
-                gameDiv.children.item(index).addEventListener("click", () =>{
+                gameDiv.children.item(index++).addEventListener("click", (e) =>{
+                    gameArr[i][j] = "x";
+                    //let active = gameDiv.closest("div").querySelector(".cell");
+                    e.target.classList.add("inactive");
+                    e.target.removeEventListener("click", () => {
 
+                    })
+                    //console.log(e.target.)
                 })
-                gameDiv.children.item(index++).textContent = gameArr[i][j];
+                //gameDiv.children.item(index++).textContent = gameArr[i][j];
             }
         }
+        //console.log(gameArr);
+
     }
     return {
         init,
-        gameArr
+        getGameArr
     }
 })();
 
 const Game = (() => {
+    Gameboard.init();
     const playerOne = Player("Shawn", "x");
     const playerTwo = Player("Charlie", "o");
 
+    const setPiece = piece => {
+
+    }
     return {
 
     }
